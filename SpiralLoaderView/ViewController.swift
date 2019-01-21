1413c8c16b83702adb45de2f8141a6f2c432f615
Ravi Tailor <ravi.tailor@jublfood.com>
2019-01-21 18:10:18 +0530

    Initial Commit

---
+++ SpiralLoaderView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpiralLoaderView
+//
+//  Created by Ravi Tailor on 21/01/19.
+//  Copyright © 2019 Jubl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
